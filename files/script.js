a4bce61849b2e5cb34222782e554417ec89c6f75
Anson Chung <ansoncht@gmail.com>
2021-09-05 00:04:14 -0700

    Template

--- files/script.js
+++ files/script.js
@@ -1,14 +1,20 @@
-// Hide/Show Navbar
-let prevScrolls = window.pageYOffset;
-window.onscroll = function() {
-    document.getElementById("nav").style.display = "block";
-    const currentScrollPos = window.pageYOffset;
-    if (prevScrolls > currentScrollPos) {
-        document.getElementById("nav").style.top = "0";
-    } else {
-        document.getElementById("nav").style.top = "-5.25em";
+document.addEventListener("DOMContentLoaded", function(){
+    el_autohide = document.querySelector('.autohide');
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+    if(el_autohide){
+        var last_scroll_top = 0;
+        window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+            if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
     }
-    prevScrolls = currentScrollPos;
-}
-
-
+});
